deaab22a92bd3fcf4a0beb5f0e1b1d7e5921e3c7
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2023-11-27 13:59:44 +0800

    wrote a drive-out program
    TODO: the auto program runner has some issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoStages/RedAllianceAutoStageProgramDefault.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoStages/RedAllianceAutoStageProgramDefault.java
@@ -17,35 +17,32 @@ public final class RedAllianceAutoStageProgramDefault extends AutoStageProgram {
 
         BezierCurve path = new BezierCurve(
                 new Vector2D(new double[] {0, 0}),
-                new Vector2D(new double[] {5, 5}),
-                new Vector2D(new double[] {10, 40}),
+                new Vector2D(new double[] {10, 5}),
+                new Vector2D(new double[] {10, 20}),
                 new Vector2D(new double[] {10, 60})
         );
         commandSegments.add(new SequentialCommandSegment(
                 path,
-                () -> {
-                    telemetry.update();
-                    telemetry.addLine("program started");
-                    telemetry.update();
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                },
                 () -> {},
-                () -> {
-                    telemetry.update();
-                    telemetry.addLine("program ended");
-                    telemetry.update();
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                },
+                () -> {},
+                () -> {},
                 () -> true,
-                0, Math.PI / 2r
+                0, 0
+        ));
+
+        path = new BezierCurve(
+                new Vector2D(new double[] {10, 60}),
+                new Vector2D(new double[] {10, 90}),
+                new Vector2D(new double[] {50, 190}),
+                new Vector2D(new double[] {70, 190})
+        );
+        commandSegments.add(new SequentialCommandSegment(
+                path,
+                () -> {},
+                () -> {},
+                () -> {},
+                () -> true,
+                0, 0
         ));
     }
 }
